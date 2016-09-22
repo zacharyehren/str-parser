ef3a7e15f40dfc2f30a13a8a3c6c8fba7d3f2baf
Zach Ehren <zachehren@Zachs-MacBook-Pro.local>
2016-09-22 15:42:23 -0700

    Add the rest of the creative parsers to program. Modified function to change the parser depending on the answers provided

--- JSON_STRProject_Updated.js
+++ JSON_STRProject_Updated.js
@@ -2,7 +2,7 @@
 /* create an if/then function that goes through the questions depending on the answers
 add those to an object
 */
-var jSon = {"creatives": [{
+var jSonClickout = {"creatives": [{
     "creative": {
         "advertiser_key": "",
         "force_click_to_play": false,
@@ -41,49 +41,178 @@ var jSon = {"creatives": [{
 "adserverRequestId": "51dd0430-e351-46e0-b86d-d29d26f1a954"
 };
 
+var jSonArticleCard = {"creatives": [{
+                "creative": {
+                    "advertiser_key": "",
+                    "force_click_to_play": true,
+                    "creative_key": "sfp-DSpDaaq3T6nkmKwQ3pJYBWTo1V",
+                    "campaign_key": "d50e187c361aba5178c3e81f",
+                    "description": "The Weather Channel Allergy Tracker provides forecasts for common allergens such as pollen and mold, factoring in weather conditions and historical data.",
+                    "media_url": "https://platform-cdn.sharethrough.com/creatives/DSpDaaq3T6nkmKwQ3pJYBWTo1V/article_content",
+                    "instant_play_mobile_count": null,
+                    "promoted_by_text": "",
+                    "share_url": "https://weather.com/forecast/allergy/l/10014:4:US",
+                    "variant_key": "DSGeoNaHvnTK7QGbEb4MeM6J79",
+                    "instant_play_mobile_url": null,
+                    "advertiser": "Sharethrough",
+                    "beacons": {
+                        "visible": [],
+                        "play": [],
+                        "completed_silent_play": [],
+                        "thirty_second_silent_play": [],
+                        "silent_play": [],
+                        "fifteen_second_silent_play": [],
+                        "click": [],
+                        "ten_second_silent_play": [],
+                        "impression": []
+                    },
+                    "custom_engagement_url": "",
+                    "thumbnail_url": "//static.sharethrough.com/creative_workflow/creative_thumbnails/82846/images/original/Screen_Shot_2016-08-09_at_1.43.46_PM.jpg",
+                    "brand_logo_url": "",
+                    "source_id": "sfp",
+                    "title": "Allergy Tracker",
+                    "icon_url": "",
+                    "custom_engagement_label": "",
+                    "action": "article"
+                },
+                "auctionWinId": "6ba04c45-2b0e-4ab0-8a88-7dcba0eb1f67",
+                "version": 1
+            }],
+            
+            "adserverRequestId": "bb625a4d-f0dc-441b-b0d2-492c5fd9eeb9"
+        }
 
+var jSonVVA = {"creatives": [{
+                "creative": {
+                    "advertiser_key": "",
+                    "force_click_to_play": false,
+                    "creative_key": "sfp-DSTPGcbr9Mi3EHWR7qnMCRYGHs",
+                    "campaign_key": "d50e187c361aba5178c3e81f",
+                    "description": "Test for HTTPS Brightcove URL v2 http://sharethrough.com/",
+                    "media_url": "https://static.sharethrough.com/sfp/hosted_video/DSTPGcbr9Mi3EHWR7qnMCRYGHs/video.mp4",
+                    "video_aspect_ratio": 0.5625,
+                    "instant_play_mobile_count": 6,
+                    "promoted_by_text": "",
+                    "share_url": "http://sharethrough.com",
+                    "variant_key": "DS6osLaaVLXbP3g4JsxLc5bN8r",
+                    "instant_play_mobile_height": 180,
+                    "instant_play_mobile_url": "https://static.sharethrough.com/sfp/instant_play_mobile/DSTPGcbr9Mi3EHWR7qnMCRYGHs/sprite0.jpg",
+                    "advertiser": "Sharethrough",
+                    "instant_play_mobile_width": 320,
+                    "beacons": {
+                        "visible": [],
+                        "play": [],
+                        "completed_silent_play": [],
+                        "thirty_second_silent_play": [],
+                        "silent_play": [],
+                        "fifteen_second_silent_play": [],
+                        "click": [],
+                        "ten_second_silent_play": [],
+                        "impression": []
+                    },
+                    "custom_engagement_url": "",
+                    "thumbnail_url": "//static.sharethrough.com/creative_workflow/creative_thumbnails/83998/images/original/sharethrough-og-hero.jpg",
+                    "brand_logo_url": "",
+                    "source_id": "sfp",
+                    "title": "Test for HTTPS Brightcove URL v2",
+                    "icon_url": "//design.sharethrough.com/assets/images/non-YT-play-squared.png",
+                    "custom_engagement_label": "",
+                    "action": "hosted-video"
+                },
+                "auctionWinId": "8b43c694-2d18-42ef-987d-9a4a45eb5c47",
+                "version": 1
+            }],
+            "adserverRequestId": "50b7501e-87ca-4d80-b510-798cfac8d73b"
+        }
+
+var jSonHostedVideo = {"creatives": [{
+                "creative": {
+                    "advertiser_key": "",
+                    "force_click_to_play": true,
+                    "creative_key": "sfp-d443d415",
+                    "campaign_key": "d50e187c361aba5178c3e81f",
+                    "description": "Description",
+                    "media_url": "https://static.sharethrough.com/bakery/hosted_video/d443d415/video.mp4",
+                    "video_aspect_ratio": 1.7778,
+                    "instant_play_mobile_count": 14,
+                    "promoted_by_text": "",
+                    "share_url": "",
+                    "variant_key": "139474",
+                    "instant_play_mobile_height": 480,
+                    "instant_play_mobile_url": "https://static.sharethrough.com/bakery/instant_play_mobile/d443d415/sprite0.jpg",
+                    "advertiser": "Sharethrough",
+                    "instant_play_mobile_width": 270,
+                    "beacons": {
+                        "visible": [],
+                        "play": [],
+                        "completed_silent_play": [],
+                        "thirty_second_silent_play": [],
+                        "silent_play": [],
+                        "fifteen_second_silent_play": [],
+                        "click": [],
+                        "ten_second_silent_play": [],
+                        "impression": []
+                    },
+                    "custom_engagement_url": "",
+                    "thumbnail_url": "//static.sharethrough.com/m/creative_thumbnails/76787/images/thumb_320/f5abeb1b0772-d_conf_myAcct_btn.jpg",
+                    "brand_logo_url": "",
+                    "source_id": "sfp",
+                    "title": "Headline",
+                    "icon_url": "//design.sharethrough.com/assets/images/non-YT-play-squared.png",
+                    "custom_engagement_label": "",
+                    "action": "hosted-video"
+                },
+                "auctionWinId": "7e18c3f7-2ce0-4980-84a6-05ee80f3c6f8",
+                "version": 1
+            }],
+            "adserverRequestId": "b850d7ab-9a52-439e-9e7b-fa3002a06da0"
+        }
 
 
 var typeOfCreative;
-var characterLength;
-var contentType;
 var promotedBy;
 var customHeadline;
 var customDescription;
 var customThumbnail;
-var premadeCreative = {};
-var customCreative = {};
+var newParse;
 
 var creative = function(){
 	typeOfCreative = prompt("Will this be a pre-made or custom creative?");
-	if(typeOfCreative === "pre-made") {
-		characterLength = prompt("What is the max character count for headlines? (40, 90, or 140)")
-		contentType = prompt("Which type of creative would you like to preview? (Website Visit Ad, Content Engagement Ad or Video View Ad?");
-		premadeCreative.characterLength = characterLength;
-		premadeCreative.contentType = contentType;
+	if(typeOfCreative.toUpperCase() == "pre-made".toUpperCase() || typeOfCreative.toUpperCase() == "pre made".toUpperCase() || typeOfCreative.toUpperCase() == "premade".toUpperCase()) {
+		var premadeCreative = prompt("Which type of creative would you like to preview? Website Visit Ad, Content Card, Video View Ad, or Hosted Video Card");
+        if(premadeCreative.toUpperCase() == "website visit ad".toUpperCase()){
+           newParse = jSonClickout;
+        } else if (premadeCreative.toUpperCase() == "content card".toUpperCase()) {
+           newParse = jSonArticleCard;
+        } else if (premadeCreative.toUpperCase() == "video view ad".toUpperCase()){
+            newParse = jSonVVA;
+        } else if (premadeCreative.toUpperCase() == "hosted video card".toUpperCase()) {
+            newParse = jSonHostedVideo;
+        } var headlineLength = prompt("How long would you like the headline to be? The options are 40, 90, 140");
+        if (headlineLength == 40) {
+            newParse.creatives[0].headline = "Lorem ipsum dolor sit amet, consectetur."
+        } else if (headlineLength == 90) {
+            newParse.creatives[0].headline = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.";
+        } else if (headlineLength == 140) {
+            newParse.creatives[0].headline = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim";
+        }
 	} 
 	else{
 		promotedBy = prompt("Awesome! What would you like the 'Promoted By' section to read? Default: 'Promoted By'");
         customHeadline = prompt("What would you like the headline to read? (140 characters max)");
         customDescription = prompt("What would you like the description to read? (180 characters max)");
         customThumbnail = prompt("Please share a URL for your thumbnail image");
-        customCreative.promotedBy = promotedBy;
-        customCreative.customHeadline = customHeadline;
-        customCreative.customDescription = customDescription;
-        customCreative.customThumbnail = customThumbnail;
+        jSonClickout.creatives[0].advertiser = promotedBy;
+        jSonClickout.creatives[0].headline = customHeadline;
+        jSonClickout.creatives[0].description = customDescription;
+        jSonClickout.creatives[0].thumbnail_url = customDescription;
+        newParse = jSonClickout;
 	}
 };
 
 
-jSon.newPremadeCreative = premadeCreative;
-jSon.newCustomCreative = customCreative;
-
-var newJsonParse = JSON.stringify(jSon);
-
-
 creative();
 
-console.log(customCreative);
+finalParse = JSON.stringify(newParse);
 
-
-
+document.write(finalParse);
